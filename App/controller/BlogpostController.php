4c2c8757a3817dad04e78a8a6807d11761022789
Anastasia Molotkova <anastasia.molotkova@kedgebs.com>
2021-06-01 21:55:57 +0200

    add blogpost update

--- App/controller/BlogpostController.php
+++ App/controller/BlogpostController.php
@@ -16,7 +16,6 @@ class BlogpostController extends AppController
         $helper = new GetPostHelper();
         $message = '';
 
-
         if (null !== ($helper->getPost('createBlogpost'))) {
 
             $blogpostModel = new BlogpostModel($_POST);
@@ -25,7 +24,6 @@ class BlogpostController extends AppController
         } else {
             echo 'please enter all information';
         }
-        print_r($_POST);
 
         $this->view->display('blogpost/createBlogpost.html.twig', ['message' => $message, 'blogpost' => $blogpost]);
     }
@@ -39,9 +37,46 @@ class BlogpostController extends AppController
 
     public function getPost($post_id)
     {
-        echo $post_id;
         $blogpostManager = new BlogpostManager();
+        //$helper = new GetPostHelper();
+        //$message = '';
         $blogpost = $blogpostManager->getPost($post_id);
+        $this->view->display('blogpost/blogpostView.html.twig', ['blogpost' => $blogpost]);
+        return $blogpost;
+    }
+
+    public function modify($post_id)
+    {
+        $blogpostManager = new BlogpostManager();
+        $helper = new GetPostHelper();
+        // $blogpost = new BlogpostManager();
+        $blogpost = $blogpostManager->getPost($post_id);
+        //var_dump($blogpost);
+
+        // $message = '';
+        if (null !== ($helper->getPost('updateBlogpost'))) {
+            //var_dump($_POST);
+            //exit;
+            $blogpostModel = new BlogpostModel($_POST);
+            //var_dump($blogpostModel);
+            //exit;
+            $blogpost = $blogpostManager->modify($blogpostModel);
+        }
+
+
+        $this->view->display('blogpost/blogpostView.html.twig', ['blogpost' => $blogpost]);
+    }
+
+    public function delete($post_id)
+    {
+        $blogpostManager = new BlogpostManager();
+        $helper = new GetPostHelper();
+        $blogpost = $blogpostManager->getPost($post_id);
+        $postId = 2;
+        if (null !== ($helper->getPost('deleteBlogpost'))) {
+            //$blogpostModel = new BlogpostModel($_POST);
+            $blogpost->delete($postId);
+        }
         $this->view->display('blogpost/blogpostView.html.twig', ['blogpost' => $blogpost]);
     }
 }
