4473e7a2d82202c843bd9acb3c41196a763a81a0
Anastasia Molotkova <anastasia.molotkova@kedgebs.com>
2021-06-24 19:15:03 +0200

    add bootstrap theme, add homepage, change base.html.twig, add public folder

---
+++ App/public/index.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__.'/../../vendor/autoload.php';
+session_start();
+date_default_timezone_set('Europe/Paris');
+$page = $_GET['action']?? 'Blogpost/listPosts';
+$action = explode('/', $page);
+$controller = '\\App\\controller\\'.$action[0].'Controller';
+$ctrl = new $controller;
+$method = $action[1];
+$params = $action[2]??'';
+if(method_exists($ctrl, $method)){
+    $ctrl->$method($params);
+} else {
+    echo 'method doesn\'t exist';
+    $method = 'add';
+    $ctrl->$method();
+}
+
